3edb75d7d7cb6851c8e4d48c6325adf94f3adf96
LaChen <sam24666806@gmail.com>
2019-02-01 22:44:39 +0800

    [add] Add unit test about common function. [add] Add layout of Bin

---
+++ UnitTest/Windows/UnitTest_CommonFunc/main.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+#include "SDEngineCommonFunction.h"
+
+using namespace SDE::Basic;
+
+int main(int argc, char **argv)
+{
+	std::cout << "SDEngine Common Func Unit Test" << std::endl;
+
+	//1. String Format.
+	{
+		int six = 666666;
+		std::string result;
+		result = StringFormat("Test six = %d and convert string", six);
+		std::cout << "Test 1 : StringFormat => " << result << ". size = "<< result.size() <<". length = "<< result.length()<< std::endl;
+	}
+
+	return 0;
+}
