ffec6bf118abb682be6e4ac916a93268ef75c716
LaChen <sam24666806@gmail.com>
2020-03-20 01:13:07 +0800

    [mod] lib destinated folder.(from lib configuration/platform to lib/platform/configuration)

--- Scripts/CopyAndroidLibs.bat
+++ Scripts/CopyAndroidLibs.bat
@@ -5,8 +5,8 @@ echo "%~dp0"
 set SDEngineRootDir=%~dp0\..
 echo "SDEngineRootDir: %SDEngineRootDir% %~dp0"
 
-xcopy /i /y %SDEngineRootDir%\Bin\UnitTest\Android\ARM\Debug\*.so %SDEngineRootDir%\UnitTest\Android\unittest_androidapp\libs\armeabi-v7a\Debug
-xcopy /i /y %SDEngineRootDir%\Bin\UnitTest\Android\ARM\Release\*.so %SDEngineRootDir%\UnitTest\Android\unittest_androidapp\libs\armeabi-v7a\Release
+xcopy /i /y %SDEngineRootDir%\Bin\UnitTest\Android\ARM\Debug\*.so %SDEngineRootDir%\UnitTest\Android\unittest_androidapp\libs\Debug\armeabi-v7a
+xcopy /i /y %SDEngineRootDir%\Bin\UnitTest\Android\ARM\Release\*.so %SDEngineRootDir%\UnitTest\Android\unittest_androidapp\libs\Release\armeabi-v7a
 
-xcopy /i /y %SDEngineRootDir%\Bin\UnitTest\Android\ARM64\Debug\*.so %SDEngineRootDir%\UnitTest\Android\unittest_androidapp\libs\arm64-v8a\Debug
-xcopy /i /y %SDEngineRootDir%\Bin\UnitTest\Android\ARM64\Release\*.so %SDEngineRootDir%\UnitTest\Android\unittest_androidapp\libs\arm64-v8a\Release
+xcopy /i /y %SDEngineRootDir%\Bin\UnitTest\Android\ARM64\Debug\*.so %SDEngineRootDir%\UnitTest\Android\unittest_androidapp\libs\Debug\arm64-v8a
+xcopy /i /y %SDEngineRootDir%\Bin\UnitTest\Android\ARM64\Release\*.so %SDEngineRootDir%\UnitTest\Android\unittest_androidapp\libs\Release\arm64-v8a
